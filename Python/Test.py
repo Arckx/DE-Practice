1483b199847e9c378f162ba130fbc83416c79161
Ahmed Rustam <ahmedkhan309.ar@gmail.com>
2026-02-14 10:16:11 +0500

    Conducted Test

---
+++ Python/Test.py
@@ -0,0 +1,70 @@
+# 
+# marks = 85
+
+# # if 50 > True else False
+    
+# def result(marks):
+#     if marks/2 > 50:
+#         return True
+#     else:
+#         return False
+    
+# print(result(marks))
+
+# basic salary = 50000
+# bonus = 10%
+
+# salary = 50000
+# bonus = 10
+
+# def bonus(sal, bonus):
+#     return sal * bonus/100
+
+# print(bonus(salary, bonus))
+
+# salary = 50000
+# bonus = 10
+
+# def bonus_calc(sal,bonus):
+#     return sal * bonus / 100
+
+# print(bonus_calc(salary,bonus))
+
+
+# tax = 5
+
+# def tax_cal(sal, tax):
+#     z = sal * tax / 100
+#     return z
+
+# t = tax_cal(salary, tax)
+# print("Tax Deductable: ", t)
+# print(salary - t)
+
+
+# def net_salary(x, y, z):
+#     return x + y - z
+
+# print(net_salary(salary, bonus_calc(salary, bonus), tax_cal(salary, tax)))
+
+
+# sal = 50000.0
+# bonus = 10
+# tax = 5
+
+# def bonus_calc(sal,bonus):
+#     return sal * bonus / 100
+# bonus_calc= bonus_calc(sal,bonus)
+
+# def tax_calc(sal,tax):
+#     return sal * tax / 100
+# tax_calc = tax_calc(sal,tax)
+
+# def calculate_net_salary(sal,bonus,tax):
+#     return sal + bonus - tax
+# calculate_net_salary = calculate_net_salary(sal,bonus_calc,tax_calc)
+
+# print("tax: ",tax_calc)
+# print("bonus: ",bonus_calc)
+# print("net_salary: ",calculate_net_salary)
+
